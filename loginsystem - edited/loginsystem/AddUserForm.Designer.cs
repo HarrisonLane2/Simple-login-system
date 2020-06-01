f349003ab87feb6f67c8aa4db0a8c5b5781d0793
Harrison Lane <43409781+HarrisonLane2@users.noreply.github.com>
2020-06-01 13:13:11 +0100

    Add files via upload

---
+++ loginsystem - edited/loginsystem/AddUserForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace loginsystem
+{
+    partial class AddUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddPasswordText = new System.Windows.Forms.Label();
+            this.AddPasswordBox = new System.Windows.Forms.TextBox();
+            this.AddUsernameBox = new System.Windows.Forms.TextBox();
+            this.AddUsernameText = new System.Windows.Forms.Label();
+            this.AddUserButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddPasswordText
+            // 
+            this.AddPasswordText.AutoSize = true;
+            this.AddPasswordText.Location = new System.Drawing.Point(21, 71);
+            this.AddPasswordText.Name = "AddPasswordText";
+            this.AddPasswordText.Size = new System.Drawing.Size(53, 13);
+            this.AddPasswordText.TabIndex = 9;
+            this.AddPasswordText.Text = "Password";
+            // 
+            // AddPasswordBox
+            // 
+            this.AddPasswordBox.Location = new System.Drawing.Point(82, 68);
+            this.AddPasswordBox.MaxLength = 15;
+            this.AddPasswordBox.Name = "AddPasswordBox";
+            this.AddPasswordBox.Size = new System.Drawing.Size(194, 20);
+            this.AddPasswordBox.TabIndex = 8;
+            this.AddPasswordBox.UseSystemPasswordChar = true;
+            this.AddPasswordBox.TextChanged += new System.EventHandler(this.AddUsernameBox_TextChanged);
+            // 
+            // AddUsernameBox
+            // 
+            this.AddUsernameBox.Location = new System.Drawing.Point(82, 26);
+            this.AddUsernameBox.MaxLength = 15;
+            this.AddUsernameBox.Name = "AddUsernameBox";
+            this.AddUsernameBox.Size = new System.Drawing.Size(194, 20);
+            this.AddUsernameBox.TabIndex = 7;
+            this.AddUsernameBox.TextChanged += new System.EventHandler(this.AddUsernameBox_TextChanged);
+            // 
+            // AddUsernameText
+            // 
+            this.AddUsernameText.AutoSize = true;
+            this.AddUsernameText.Location = new System.Drawing.Point(21, 29);
+            this.AddUsernameText.Name = "AddUsernameText";
+            this.AddUsernameText.Size = new System.Drawing.Size(55, 13);
+            this.AddUsernameText.TabIndex = 6;
+            this.AddUsernameText.Text = "Username";
+            // 
+            // AddUserButton
+            // 
+            this.AddUserButton.Location = new System.Drawing.Point(121, 107);
+            this.AddUserButton.Name = "AddUserButton";
+            this.AddUserButton.Size = new System.Drawing.Size(75, 23);
+            this.AddUserButton.TabIndex = 10;
+            this.AddUserButton.Text = "Add User";
+            this.AddUserButton.UseVisualStyleBackColor = true;
+            this.AddUserButton.Click += new System.EventHandler(this.AddUserButton_Click);
+            // 
+            // AddUserForm
+            // 
+            this.AcceptButton = this.AddUserButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 142);
+            this.Controls.Add(this.AddUserButton);
+            this.Controls.Add(this.AddPasswordText);
+            this.Controls.Add(this.AddPasswordBox);
+            this.Controls.Add(this.AddUsernameBox);
+            this.Controls.Add(this.AddUsernameText);
+            this.Name = "AddUserForm";
+            this.Text = "AddUser";
+            this.Load += new System.EventHandler(this.AddUserForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AddPasswordText;
+        private System.Windows.Forms.TextBox AddPasswordBox;
+        private System.Windows.Forms.TextBox AddUsernameBox;
+        private System.Windows.Forms.Label AddUsernameText;
+        private System.Windows.Forms.Button AddUserButton;
+    }
+}
---
+++ loginsystem - edited/loginsystem/AddUserForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace loginsystem
+{
+    public partial class AddUserForm : Form //declaring the adduserform
+    {
+        UserLogInForm login;      
+        public AddUserForm(UserLogInForm y)
+        {
+            login = y;
+            InitializeComponent();//loading the form
+        }
+
+        private void AddUsernameBox_TextChanged(object sender, EventArgs e) //run when the text in addusernamebox is changed so they cant submit the form empty
+        {
+            AddUserButton.Enabled = (AddUsernameBox.Text.Trim() != "" && AddPasswordBox.Text.Trim() != "");//takeing the text that was inputed in the textboxes 
+        }
+
+        private void AddUserButton_Click(object sender, EventArgs e)//run when adduserbutton is clicked on
+        {
+            string[] credentials = { AddUsernameBox.Text, AddPasswordBox.Text };//storing the text box text from the form under credidentials
+            if (File.Exists(login.path + "/user.login"))//checking to see if the file is there
+                File.Delete(login.path + "/iser.login");//delete the file if there is one 
+            File.WriteAllLines(login.path + "/user.login", credentials);//input the users credidentials into the file 
+            login.userExists = true;// user now is in the system
+            MessageBox.Show("The user " + credentials[0] + " has been added");//message bos saying "The user "the users username" has been added"
+            this.Close();//close the form
+        }
+
+        private void AddUserForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
